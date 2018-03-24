b4d3a5d5fdd1834df77f9f77c9e5792a33c4baca
DenisLapi <denislapidenis@gmail.com>
2018-03-24 17:17:43 +0200

    Template finished v0.1

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,5 @@
+// Responsive menu
+
 $(".menu-btn").on("click", function() {
 	var navigation = $(".right-side ul");
 	if(!navigation.attr("nav-active")) {
@@ -8,3 +10,11 @@ $(".menu-btn").on("click", function() {
 		navigation.removeAttr("nav-active");
 	}
 });
+
+// Slider width and sizes
+var slides = $(".slides");
+var slide = $(".slides .slide-li");
+var slideCount = slides.children().length;
+var slideWidth = (slide.width() * slideCount) + slide.width() + 100;
+
+// slides.width(slideWidth);
